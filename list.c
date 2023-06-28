2742dfdb659a9de8d5910ddc115ea2bc9d7cdcde
pattonbrown <2998691695@qq.com>
2023-06-28 20:14:26 +0800

    完成链表的合并和删除

--- list.c
+++ list.c
@@ -31,3 +31,33 @@ void appendNode(PNode* head,PNode newNode){
     }
 }
 
+void mergeNodes(PNode head){
+    PNode current=head;
+    while(current!=NULL){
+        PNode runner=current->next;
+        PNode prevRunner=current;
+        while(runner!=NULL){
+            if(strcmp(current->data.name,runner->data.name)==0){
+                current->data.totalCount+=runner->data.totalCount;
+                prevRunner->next=runner->next;
+                PNode temp=runner;
+                runner=runner->next;
+                destoryNode(temp);
+            }else{
+                prevRunner=runner;
+                runner=runner->next;
+            }
+        }
+        current=current->next;
+    }
+}
+
+void destoryList(PNode* head){
+    PNode current=*head;
+    while(current!=NULL){
+        PNode temp=current;
+        current=current->next;
+        destoryNode(temp);
+    }
+    *head=NULL;
+}
